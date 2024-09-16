9e6b8de4d77ab61a797317b1a55f1b809b8790fc
Shashi Kumar <shashikr2201@gmail.com>
2024-09-16 06:35:01 +0000

    flash attention

--- train_gpt2.py
+++ train_gpt2.py
@@ -30,11 +30,12 @@ class CausalSelfAttentionBlock(nn.Module): # Multi Head Attention
         k = k.view(B, T, self.n_head, C//self.n_head).transpose(1, 2)
         v = v.view(B, T, self.n_head, C//self.n_head).transpose(1, 2)
 
-        att = q@k.transpose(-1, -2) * (1.0/math.sqrt(k.size(-1)))               # (B, n_head, T, T)
-        att = att.masked_fill(self.bias[:,:,:T,:T]==0, float('-inf'))
-        att = F.softmax(att, dim=-1)
+        # att = q@k.transpose(-1, -2) * (1.0/math.sqrt(k.size(-1)))               # (B, n_head, T, T)
+        # att = att.masked_fill(self.bias[:,:,:T,:T]==0, float('-inf'))
+        # att = F.softmax(att, dim=-1)
+        # y = att @ v
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
 
-        y = att @ v
         y = y.transpose(1, 2).contiguous().view(B, T, C)
         y = self.c_proj(y)
         return y
